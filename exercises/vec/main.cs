4dbc8b9236b28fc9c311eaca75e8a5f4e72cdfab
Mikkel <mikkelodeonn@icloud.com>
2024-02-07 15:04:57 +0100

    -

---
+++ exercises/vec/main.cs
@@ -0,0 +1,19 @@
+public class vec{
+	public double x,y,z; 
+
+	// Constructors:
+
+	public vec(){
+		x=y=z=0; 
+		}
+
+	public vec(double x, double y, double z){ 
+		this.x=x; this.y=y; this.z=z; 
+		}
+
+	// Operators:
+
+
+	// Methods:
+	
+}
