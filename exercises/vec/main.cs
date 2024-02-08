79eb1d30a610dcd2f76c7601b49f2ae7c8713d7d
Mikkel <mikkelodeonn@icloud.com>
2024-02-08 17:01:06 +0100

    -

--- exercises/vec/main.cs
+++ exercises/vec/main.cs
@@ -1,19 +1,46 @@
-public class vec{
-	public double x,y,z; 
+using System;
+using static System.Console;
+using static System.Math;
 
-	// Constructors:
+class main{
 
-	public vec(){
-		x=y=z=0; 
+	static int Main(){
+
+		vec v = new vec(1,2,3);
+		vec u = new vec(4,5,6);
+
+		v.print("Vector v = ");
+		u.print("Vector u = ");
+
+		vec sum = u + v;
+		sum.print("u + v = ");
+
+		vec diff = u - v;
+		diff.print("u - v = ");
+		
+		double c = 3;
+		vec prod = c * v; 
+		prod.print("c * v = ");
+
+		vec neg = -v;
+		neg.print("-v = ");
+
+		WriteLine("");
+
+		double dot_product = vec.dot(u,v);
+		WriteLine($"Dot product of u and v: {dot_product}\n");
+
+		double norm = vec.norm(u);
+		WriteLine($"Norm of u: {norm}\n");
+
+		if(u.approx(v)){
+		
+			WriteLine($"u is approximately equal to v\n");
+		}		
+		else{
+			WriteLine($"u is not approximately equal to v\n");
 		}
 
-	public vec(double x, double y, double z){ 
-		this.x=x; this.y=y; this.z=z; 
-		}
-
-	// Operators:
-
-
-	// Methods:
-	
+		return 0;
+	}
 }
