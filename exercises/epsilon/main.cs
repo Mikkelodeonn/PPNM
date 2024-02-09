8de8cf018eb6272e78b71d094b5bc5283833adee
Mikkel <mikkelodeonn@icloud.com>
2024-02-09 17:23:05 +0100

    -

--- exercises/epsilon/main.cs
+++ exercises/epsilon/main.cs
@@ -3,14 +3,15 @@ using static System.Console;
 
 class main{
 	
-	public int Main(){
+	public static int Main(){
 
 	int i = 1; while(i + 1 > i) { i++; }
 	WriteLine($"Maximum representable integer: {i}");
 	
 	int j = 1; while(j - 1 < j) { j--; }
 	WriteLine($"Minimum representable integer: {j}");
-
+	
+	return 0;
 	}
 
 }
