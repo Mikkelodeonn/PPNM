6544a1ca713bb7c3e73d733b451458aec1d5ed35
Mikkel <mikkelodeonn@icloud.com>
2024-02-12 11:22:13 +0100

    -

---
+++ exercises/input-output/task2/main.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+public static int Main(){
+char[] split_delimiters = {' ','\t','\n'};
+var split_options = StringSplitOptions.RemoveEmptyEntries;
+for( string line = ReadLine(); line != null; line = ReadLine() ){
+	var numbers = line.Split(split_delimiters,split_options);
+	foreach(var number in numbers){
+		double x = double.Parse(number);
+		Error.WriteLine($"{x} {Sin(x)} {Cos(x)}");
+                }
+        }
+return 0;
+}
+}
