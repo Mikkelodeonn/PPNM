08b35daa8eefaf93d1507e6c798784d98e4b19f8
Mikkel <mikkelodeonn@icloud.com>
2024-02-15 09:27:22 +0100

    -

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -13,32 +13,32 @@ public static int Main(){
 
 
 	complex a = cmath.sqrt(cmath.pow(i,2));
-	WriteLine($"The squareroot of -1 is: {a}\n");
-	//WriteLine($"Check: {complex.approx(a/i,1)}\n");
+	WriteLine($"The squareroot of -1 is: {a}");
+	WriteLine($"Check with approx function: {cmath.approx(1, a/i)}\n");
 
 	complex b = cmath.sqrt(i);
-	WriteLine($"The squareroot of i is: {b}\n");
-	//WriteLine($"Check: {cmath.approx(b, 1/(Math.Sqrt(2)) + i/(Math.Sqrt(2)))}\n");
+	WriteLine($"The squareroot of i is: {b}");
+	WriteLine($"Check with approx function: {cmath.approx(1/(Math.Sqrt(2)), b-i/(Math.Sqrt(2)))}\n");
 
 	complex c = cmath.exp(i);
-	WriteLine($"exp(i) = {c}\n");
-	//WriteLine($"Check: {c,(0.54+0.84*i)}\n");
+	WriteLine($"exp(i) = {c}");
+	WriteLine($"Check with approx function: {cmath.approx(0.540302305868, c-0.841470984807*i)}\n");
 
 	complex d = cmath.exp(i*Math.PI);
-	WriteLine($"exp(i*pi) = {d}\n");
-	//WriteLine($"Check: {cmath.approx(d,-1)}\n");
+	WriteLine($"exp(i*pi) = {d}");
+	WriteLine($"Check with approx function: {cmath.approx(-1, d)}\n");
 
 	complex e = cmath.pow(i,i);
-	WriteLine($"i to the power of i is: {e}\n");
-	//WriteLine($"Check: {cmath.approx(e,0,208)}\n");
+	WriteLine($"i to the power of i is: {e}");
+	WriteLine($"Check with approx function: {cmath.approx(0.207879576350, e)}\n");
 
 	complex f = cmath.log(i);
-	WriteLine($"ln(i) = {f}\n");
-	//WriteLine($"Check: {f,i*Math.PI/2}\n");
+	WriteLine($"ln(i) = {f}");
+	WriteLine($"Check with approx function: {cmath.approx(Math.PI/2,f/i)}\n");
 
 	complex g = cmath.sin(i*Math.PI);
-	WriteLine($"sin(i*pi) = {g}\n");
-	//WriteLine($"Check: {cmath.approx(g,11.55*i)}\n");
+	WriteLine($"sin(i*pi) = {g}");
+	WriteLine($"Check with approx function: {cmath.approx(11.548739357257,g/i)}\n");
 
 return 0;
 } // Main
