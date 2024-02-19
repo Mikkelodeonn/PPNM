d3324f6d7999aa6d025a49a5715d367932febcd0
Mikkel <mikkelodeonn@icloud.com>
2024-02-19 13:29:03 +0100

    -

--- exercises/plots/main.cs
+++ exercises/plots/main.cs
@@ -7,11 +7,11 @@ public static int Main(){
 		WriteLine($"{x} {sfuns.erf(x)}");
 		}
 	WriteLine("\n");
-	for(double y=1;y<=4;y+=1.0/8){
+	for(double y=0.1;y<=5;y+=1.0/8){
 		WriteLine($"{y} {sfuns.gamma(y)}");
 		}
 	WriteLine("\n");
-	for(double z=1;z<=4;z+=1.0/8){
+	for(double z=0.1;z<=5;z+=1.0/8){
 		WriteLine($"{z} {sfuns.lngamma(z)}");
 		}
 return 0;
