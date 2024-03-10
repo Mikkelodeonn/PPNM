11594fae29c3ecb69b7be798e0f97a0fb99b6d82
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-03-10 17:00:24 +0100

    -

--- homeworks/splines/main.cs
+++ homeworks/splines/main.cs
@@ -5,27 +5,49 @@ static int Main(){
     int index = 10;
     double[] x = new double[index];
     double[] y = new double[index];
+    double[] y_int = new double[index];
+    double[] dy = new double[index];
+    //WriteLine("x sin(x)");
     for(int i=0 ; i<index ; i++){
         x[i] = i; 
         y[i] = Sin(x[i]); 
         WriteLine($"{x[i]} {y[i]}");
     }
-    WriteLine("\n\n");
-    for(double j=0 ; j<=index-1 ; j+=0.25){
-        double s_lin = spline.linear(x,y,j);
-        double s_lin_int = spline.linear_integrate(x,y,j);
-        WriteLine($"{j} {s_lin} {s_lin_int}"); 
+    WriteLine("\n\n\n"); //x integral of sin(x)");
+    for(int i=0 ; i<index ; i++){ 
+        y_int[i] = 1 - Cos(x[i]); 
+        WriteLine($"{x[i]} {y_int[i]}");
     }
-
-    WriteLine("\n\n");
-    (double[] b, double[] c) = spline.quad_build(x,y);
-
-    WriteLine("\n\n");
-    for(double h=0 ; h<index-1 ; h+=0.125){
-        double s_quad = spline.quad_eval(x,y,b,c,h);
-        double s_quad_int = spline.quad_integrate(x,y,b,c,h);
-        double ds_dx = spline.quad_derivative(x,b,c,h);
-        WriteLine($"{h} {s_quad} {s_quad_int} {ds_dx}"); 
+    WriteLine("\n\n\n"); //x derivative of sin(x)
+    for(int i=0 ; i<index ; i++){
+        dy[i] = Cos(x[i]); 
+        WriteLine($"{x[i]} {dy[i]}");
+    }
+    var (b,c) = spline.quad_build(x,y);
+    WriteLine("\n\n\n0:Index 1:Linear spline");
+    for(double j=0 ; j<=index-1 ; j+=1.0/16){
+        double s_lin = spline.linear(x,y,j);
+        WriteLine($"{j} {s_lin}"); 
+    }
+      WriteLine("\n\n\n0:Index 2:Linear integral");
+    for(double j=0 ; j<=index-1 ; j+=1.0/16){
+        double s_lin_integral = spline.linear_integrate(x,y,j);
+        WriteLine($"{j} {s_lin_integral}"); 
+    }
+      WriteLine("\n\n\n0:Index 3:Quadratic spline");
+    for(double j=0 ; j<=index-1 ; j+=1.0/16){
+        double s_quad = spline.quad_eval(x,y,b,c,j);
+        WriteLine($"{j} {s_quad}"); 
+    }
+      WriteLine("\n\n\n0:Index 4:Quadratic integral");
+    for(double j=0 ; j<=index-1 ; j+=1.0/16){
+        double s_quad_integral = spline.quad_integrate(x,y,b,c,j);
+        WriteLine($"{j} {s_quad_integral}"); 
+    }
+      WriteLine("\n\n\n0:Index 5:Quadratic derivative");
+    for(double j=0 ; j<=index-1 ; j+=1.0/16){
+        double s_quad_derivative = spline.quad_derivative(x,b,c,j);
+        WriteLine($"{j} {s_quad_derivative}"); 
     }
 return 0;
 }
--- homeworks/splines/spline.cs
+++ homeworks/splines/spline.cs
@@ -12,7 +12,8 @@ public static double linear_integrate(double[] x, double[] y, double z){
     int i = locate_index.binsearch(x,z);
     double dx=x[i+1]-x[i]; 
     double dy=y[i+1]-y[i];
-    double integral = y[i] * (z - x[0]) + dy/dx * (Pow((z - x[0]),2))/2;
+    double p = dy/dx;
+    double integral = y[i] * (z - x[0]) + p * (Pow((z - x[0]),2))/2;
 return integral;
 } // linear_integral
 
@@ -22,11 +23,11 @@ public static (double[],double[]) quad_build(double[] x, double[] y){
     double[] b = new double[x.Length];
     double[] c = new double[x.Length];
     double[] p = new double[x.Length];
+    c[0] = 0;
     for(int i=0 ; i<x.Length-1 ; i++){
         double dx = x[i+1] - x[i]; if(!(dx>0)) throw new Exception("x not continuous...");
         double dy = y[i+1] - y[i];
         p[i] = dy/dx;
-        c[0] = 0;
         c[i+1] = (1/dx) * (p[i+1] - p[i] - c[i]*dx);
         b[i] = p[i] - c[i]*dx;
     }
@@ -40,14 +41,14 @@ return s;
 } // quad_eval
 
 public static double quad_derivative(double[] x, double[] b, double[] c, double z){
-    int index = locate_index.binsearch(x,z);
-    double ds_dx = b[index] + 2*c[index]*(z - x[index]);
+    int i = locate_index.binsearch(x,z);
+    double ds_dx = b[i] + 2*c[i]*(z-x[0]);
 return ds_dx;
 } // quad_integrate
 
 public static double quad_integrate(double[] x, double[] y, double[] b, double[] c, double z){
-    int index = locate_index.binsearch(x,z);
-    double s_int = z*y[index] + (1/2)*b[index]*z*(z-2*x[index]) + (1/3)*c[index]*z*(Pow(z,2)-3*z*x[index]+3*Pow(x[index],2));
+    int i = locate_index.binsearch(x,z);
+    double s_int = y[i] * (z-x[0]) + b[i] * Pow((z-x[0]),2)/2 + 2*c[i] * Pow((z-x[0]),3)/3;
 return s_int;
 } // quad_derivative
 } // class interpol
