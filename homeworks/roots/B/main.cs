d19830f527bc9c8247e56c09c581112974e93f13
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-04-09 18:46:28 +0200

    -

--- homeworks/roots/B/main.cs
+++ homeworks/roots/B/main.cs
@@ -3,7 +3,7 @@ using static System.Console;
 
 public class main{
 
-public static vector radial_wave_function(double e, double rmin, double rmax){
+public static (genlist<double>, genlist<vector>) radial_wave_function(double e, double rmin, double rmax){
 vector sch = new vector(rmin - rmin * rmin, 1 - 2 * rmin);
 Func<double,vector,vector> schrodinger = (r,y) => {
     vector res = new vector(2);
@@ -13,26 +13,20 @@ Func<double,vector,vector> schrodinger = (r,y) => {
 };
 double acc = 1e-3;
 double eps = 1e-3;
-var (x_sol, y_sol) = RK.driver(schrodinger, (rmin,rmax), sch, acc, eps);
-return y_sol[0];
+var (pos, wave_function) = RK.driver(schrodinger, (rmin,rmax), sch, acc, eps);
+return (pos, wave_function);
 } // radial wave function
 
-public static double states(double rmin, double rmax) {
-Func<vector,vector> M = delegate(vector e) {
-    var res = radial_wave_function(e[0], rmax, rmin);
-    return res;
-    };
-double eps = 1e-3;
-var energies = roots.newton(M, new vector(-1.0), eps);
-return energies[0];
-} // states
+
 
 public static void Main(){
 double rmin = 1e-3;
 double rmax = 8;
 
-double E0 = states(rmin, rmax);
-WriteLine($"The lowest energy E0, found for rmin = {rmin} and rmax = {rmax}: {E0} Hartree.");
+(genlist<double> a, genlist<vector> b) = radial_wave_function(1, rmin, rmax);
+WriteLine($"The lowest energy E0, found for rmin = {rmin} and rmax = {rmax}: {a[0]} Hartree.");
 WriteLine("Exact result: -0.5 Hartree.");
+WriteLine("\n\n\n");
+for(int i=0;i<a.size;i++){WriteLine($"{a[i]}   {b[i][0]}    {b[i][1]}");}
 } // Main
 } // class main
