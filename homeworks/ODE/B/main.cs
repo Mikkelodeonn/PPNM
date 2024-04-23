0ea95cc1dffa33d6422975229b6a26614bc9da19
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-04-23 11:29:03 +0200

    -

--- homeworks/ODE/B/main.cs
+++ homeworks/ODE/B/main.cs
@@ -7,7 +7,7 @@ class main{
 public static void Main(){
 
 	double b=0.25, c=5;
-    double k=10, m=10;
+  double k=10, m=10;
 
 	Func<double,vector,vector> F = delegate(double t,vector y){ // implementation of scipy example of damped pendulum
         double theta=y[0], omega=y[1];
@@ -21,8 +21,8 @@ public static void Main(){
 
 	double start=0, stop=10;
 	vector ystart = new vector(PI-0.1,0.0);
-  Func <double,vector> F_interpolant = RK.ode_interpolant(F,(start,stop), ystart);
-  Func <double,vector> f_interpolant = RK.ode_interpolant(f,(start,stop), ystart);
+  Func <double,vector> F_interpolant = RK.ode_interpolant(F, (start,stop), ystart);
+  Func <double,vector> f_interpolant = RK.ode_interpolant(f, (start,stop), ystart);
 
 	for(double i=0.0 ; i<10 ; i+=1.0/16)
 		WriteLine($"{i} {F_interpolant(i)[0]}"); 
@@ -36,7 +36,26 @@ public static void Main(){
   for(double i=0.0 ; i<10 ; i+=1.0/16)
     WriteLine($"{i} {f_interpolant(i)[1]}"); 
   WriteLine("\n\n\n");
-  
+
+  double eps = 0.0;
+  Func<double,vector,vector> newtonian_motion = delegate(double t, vector y){
+    double y0 = y[0], y1 = y[1];
+    return new vector(y1, 1 - y0 + eps * y0 * y0);
+  };
+  vector init1 = new vector(1, 0);
+  vector init2 = new vector(1, -0.5);
+  Func<double,vector> f_circular = RK.ode_interpolant(newtonian_motion, (0,36), init1);
+  Func<double,vector> f_eliptical = RK.ode_interpolant(newtonian_motion, (0,36), init2);
+
+  for(double i=0.0 ; i<36 ; i+=1.0/16){
+    double u = f_eliptical(i)[0], phi = f_eliptical(i)[1];
+    WriteLine($"{(1/u)*Cos(phi)} {(1/u)*Sin(phi)}");
+  }
+  WriteLine("\n\n\n");
+  for(double i=0.0 ; i<36 ; i+=1.0/16){
+    double u = f_circular(i)[0], phi = f_circular(i)[1];
+    WriteLine($"{(1/u)*Cos(phi)} {(1/u)*Sin(phi)}");
+  }
    
 } // Main
 } // class main
