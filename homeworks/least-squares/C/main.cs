08f628b377cad3c7dfb8e40ead2e397a7fff0edc
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-06-15 16:20:56 +0200

    -

--- homeworks/least-squares/C/main.cs
+++ homeworks/least-squares/C/main.cs
@@ -10,38 +10,38 @@ public static int Main(){
     vector dy = new vector(y.size); // error of activity
     vector lny = new vector(y.size); // log of activity
     vector dlny = new vector(y.size); // error of log(y)
-
-
     for(int i=0 ; i<y.size ; i++){
         dy[i] = y[i] * 0.05; // assuming errors of 5%
         lny[i] = Math.Log(y[i]); // calculating ln(y)
         dlny[i] = dy[i] / y[i];
     }
-
-    x.print("Time [days]:                  ");
-    y.print("Activity of ThX [rel. u.]:    ");
-    dy.print("Error of the activity (5%):   ");
-    lny.print("Log(y):                       ");
-    dlny.print("Error of log(y):              ");
-
     Func<double,double>[] decay_func = new Func<double,double> [] {z => 1, z => -z};
-
     var (coeffs, cov_mat) = lslib.lsfit(decay_func, x, lny, dlny);
-
-    coeffs.print("\nFitting parameters (a,b):\n");
-    //cov_mat.print("Covariance matrix:\n");
-
-    WriteLine($"\nHalf-Life of ThX: ln(2)/b = {Round(Log(2)/coeffs[1],3)} days.");
-    WriteLine("Table value (modern): 3.6 days");
+    vector fit_err = new vector(cov_mat.size1);
+    for(int i=0 ; i<cov_mat.size1 ; i++){
+        fit_err[i] = Sqrt(cov_mat[i,i]);
+    }
 
     WriteLine("\n Fitting parameter values (for plotting):\n\n");
+
     vector y_fit = new vector(y.size);
+    vector y_plus = new vector(y.size);
+    vector y_minus = new vector(y.size);
+
     for(int j=0 ; j<y.size ; j++){
         y_fit[j] = Exp(coeffs[0] - coeffs[1]*x[j]);
         WriteLine($"{x[j]}  {y_fit[j]}");
     }
-
+    WriteLine("\n\n\n");
+    for(int j=0 ; j<y.size ; j++){
+        y_plus[j] = Exp((coeffs[0]+fit_err[0]) - (coeffs[1]+fit_err[1])*x[j]);
+        WriteLine($"{x[j]}  {y_plus[j]}");
+    }
+    WriteLine("\n\n\n");
+    for(int j=0 ; j<y.size ; j++){
+        y_minus[j] = Exp((coeffs[0]-fit_err[0]) - (coeffs[1]-fit_err[1])*x[j]);
+        WriteLine($"{x[j]}  {y_minus[j]}");
+    }
 return 0;
-}
-
+} // Main
 } // class main
