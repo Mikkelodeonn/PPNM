bc0b1bc7761ae6ebd129997f69d1f49e8ac69545
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-06-19 16:04:55 +0200

    -

--- exam/lanczos/A/main.cs
+++ exam/lanczos/A/main.cs
@@ -1,4 +1,5 @@
 using System;
+using static System.Console;
 static class main{
 
 public static matrix CreateRandomSymmetricMatrix(int N){
@@ -15,19 +16,35 @@ return A;
 }
 public static void Main(){
 
+WriteLine("---------------Testing of the implemented Lanczos tridiagonalization algorithm for real symmetric matrices---------------");
+
 matrix A = CreateRandomSymmetricMatrix(5);
 
 var (V,T) = diag.lanczos(A);
 
-A.print("A:");
+A.print("\nRandomly generated symmetric matrix A:");
 
-V.print("V:"); 
+T.print("\nResulting tridiagonal real symmetric matrix T:");
 
-T.print("T:");
+V.print("\nCorresponding (unitary) matrix with orthonormal columns V:"); 
+
+WriteLine("\n---------------Checking the identites of a succesful tridiagonalization -> A = V * T * V^T & T = V^T * A * V---------------");
+
+matrix Y = V*T*V.transpose();
 
 matrix Q = V.transpose()*A*V;
 
-Q.print("VAV=T:");
+Q.print("\nT = V^T * A * V:");
+
+bool T_check = T.approx(Q, acc:1e-6);
+
+WriteLine($"\nMatrix T is equal to V^T * A * V (within acc=1e-6): {T_check}");
+
+Y.print("\nA = V * T * V^T:");
+
+bool A_check = A.approx(Y, acc:1e-6);
+
+WriteLine($"\nMatrix A is equal to V * T * V^T (within acc=1e-6): {A_check}");
 
 } // Main
 } // class main
